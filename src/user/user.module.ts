1fa7d991939ffefb61daa982196836aa3b3db955
shreyesh kolhe <shreyesh.kolhe@etevatech.com>
2023-03-19 17:36:15 +0530

    secure api done

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtStrategy } from 'src/utils/jwt.strategy';
 import { LocalStrategy } from 'src/utils/local.strategy';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
@@ -12,6 +13,6 @@ import { UserService } from './user.service';
     signOptions: { expiresIn: '220d' },
   }),],
   controllers: [UserController],
-  providers: [UserService, LocalStrategy,],
+  providers: [UserService, LocalStrategy, JwtStrategy],
 })
 export class UserModule { }
