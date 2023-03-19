01d5b1259f308a59b6dbcfaf6fcfd5df59d3da0d
shreyesh kolhe <shreyesh.kolhe@etevatech.com>
2023-03-19 08:09:41 +0530

    getAll user data

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
 
 @Injectable()
 export class UserService {
+
+  constructor(private prismaService : PrismaService) {}
+
+
   getHello(): string {
     return 'Hello World!';
   }
 
   async getAllUser(){
-    return 'Get All User';
+    return this.prismaService.user.findMany();
   }
 
   async createUser(){
