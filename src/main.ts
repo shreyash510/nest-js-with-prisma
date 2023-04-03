9bc2a70d77db230bb054404865177a03b4632d44
shreyesh kolhe <shreyashkolhe2001@gmail.com>
2023-04-03 17:13:26 +0530

    swagger integrate

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   // NestFactory provide few methods that allows creating application instance.
   const app = await NestFactory.create(AppModule);
   // setGlobalPrefix allows to define a global URL prefix for all the routes in the application
+  const config = new DocumentBuilder()
+  .setTitle('Cats example')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   app.setGlobalPrefix('api'); 
+
   await app.listen(3000);
 }
 bootstrap();
